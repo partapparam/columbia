11175a4d364968308ad4481a9136ca2d9c5dc8fc
partapparam <paramsingh1961@gmail.com>
2023-12-15 22:49:22 -0800

    setting up firebase for auth for the admin

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import "./App.css"
 import { LetterProvider } from "./providers/letterContext"
 import AdminTable from "./components/adminTable"
 import AdminEditor from "./components/adminEditor"
+import AdminLogin from "./components/adminLogin"
 
 const App = () => {
   return (
@@ -14,8 +15,9 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Letter />} />
           <Route path="/thank-you" element={<Home />} />
+          <Route path="/login" element={<AdminLogin />} />
           <Route path="/admin" element={<Admin />}>
-            <Route path="editor/letter" element={<AdminEditor />} />
+            <Route path="editor/:type" element={<AdminEditor />} />
             <Route index element={<AdminTable />} />
           </Route>
         </Routes>
